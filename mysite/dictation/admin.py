0ecb0a22747b5a8aa62ca5af97354e9372369256
Cao Ke <caokecs@outlook.com>
2024-01-07 04:24:40 +0800

    add trigger

--- mysite/dictation/admin.py
+++ mysite/dictation/admin.py
@@ -1,48 +1,47 @@
 from django.contrib import admin
-from .models import dictation_task_teacher,content,dictation_task_student
-from django.contrib.auth.models import Group,User
-from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import task_teacher,content,task_student,Class,student
 
 
+def fenfa(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.fenfa()
 
+fenfa.short_description = "分发任务"
 
-
-class dictation_task_teacherAdmin(admin.ModelAdmin):
+class task_teacherAdmin(admin.ModelAdmin):
 
     fieldsets = [
-        (None,               {'fields': ['task_name']}),
-        ('内容', {'fields': ['contents']}),
-        ('班级和学生', {'fields': ['groups','users']}),
-        ('时间', {'fields': ['task_pub_time','task_start_time','task_end_time']}),
+        (None,              {'fields': ['task_name']}),
+        ('内容',            {'fields': ['contents']}),
+        ('班级和学生',      {'fields': ['classes','students']}),
+        ('时间',            {'fields': ['task_pub_time','task_start_time','task_end_time']}),
     ]
-    filter_horizontal = ('contents','groups','users') 
-    list_display = ('task_name','status','task_pub_time','task_start_time','task_end_time','remain_time','get_contents')
+    filter_horizontal = ('contents','classes','students') 
+    list_display = ('task_name','status','task_pub_time','task_start_time','task_end_time','remain_time','get_contents','get_classes','get_students')
     list_filter = ["task_pub_time","status"]
     search_fields = ["task_name"]
 
-class dictation_task_studentAdmin(admin.ModelAdmin):
-    list_display = ('task_name','status','task_start_time','task_end_time','remain_time')
+class task_studentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['task_name']}),
+        ('内容',            {'fields': ['contents']}),
+        ('时间',            {'fields': ['task_start_time','task_end_time']}),
+    ]
+    filter_horizontal = ('contents',)
+    list_display = ('task_name','status','task_start_time','task_end_time','remain_time','student')
     list_filter = ['task_start_time',"status"]
     search_fields = ["task_name"]
 
-class UserAdmin(DefaultUserAdmin):
-    list_display = ('username','first_name','get_groups')
-    def get_groups(self, obj):
-        return ", ".join([group.name for group in obj.groups.all()])
-    get_groups.short_description = '班级'
-    list_filter = ["groups"]
-    search_fields = ["username","first_name"]
 
+admin.site.register(task_teacher,task_teacherAdmin)
 
-admin.site.register(dictation_task_teacher,dictation_task_teacherAdmin)
-
-admin.site.register(dictation_task_student,dictation_task_studentAdmin)
+admin.site.register(task_student,task_studentAdmin)
 
 admin.site.register(content)
 
-if admin.site.is_registered(User):
-    admin.site.unregister(User)
+admin.site.register(Class)
 
-admin.site.register(User, UserAdmin)
+admin.site.register(student)
+
 
 
--- mysite/dictation/models.py
+++ mysite/dictation/models.py
@@ -3,13 +3,15 @@ from django.utils import timezone
 import datetime
 from django.contrib import admin
 from django.contrib.auth.models import Group,User
-
-
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 class content(models.Model):
-    content_name = models.CharField('古诗词名称',max_length=200,null=False)
-    content_age = models.CharField('古诗词朝代',max_length=200,default='未知')
-    content_author = models.CharField('古诗词作者',max_length=200,default='未知')
-    content_txt = models.TextField('古诗词内容',max_length=20000,null=False)
+
+    id              = models.AutoField(primary_key=True)
+    content_name    = models.CharField('古诗词名称',max_length=200,null=False)
+    content_age     = models.CharField('古诗词朝代',max_length=200,default='未知')
+    content_author  = models.CharField('古诗词作者',max_length=200,default='未知')
+    content_txt     = models.TextField('古诗词内容',max_length=20000,null=False)
 
     class Meta:
         verbose_name = "古诗词库"
@@ -21,16 +23,17 @@ class content(models.Model):
 def get_default_end_time(days=0, hours=0, minutes=0):
     return timezone.now() + datetime.timedelta(days=days, hours=hours, minutes=minutes)
 
-class dictation_task_teacher(models.Model):
-    
+class task_teacher(models.Model):
+
+    id              = models.AutoField(primary_key=True)
     task_name       = models.CharField('默写任务名称',max_length=200,default='默写任务')
     task_pub_time   = models.DateTimeField('任务发布时间',default=get_default_end_time())# task_pub_time 无需输入 auto_now_add 自动添加
     contents        = models.ManyToManyField(content, verbose_name='古诗词库')
     task_start_time = models.DateTimeField('任务开始时间',default=get_default_end_time())
     task_end_time   = models.DateTimeField('任务结束时间',default=get_default_end_time(hours=1))
     status          = models.CharField('状态',max_length=200,default='未开始')
-    groups          = models.ManyToManyField(Group,verbose_name='指定班级',blank=True)
-    users           = models.ManyToManyField(User,verbose_name='指定学生',blank=True)
+    classes         = models.ManyToManyField('Class',verbose_name='指定班级',blank=True)
+    students        = models.ManyToManyField('student',verbose_name='指定学生',blank=True)
     class Meta:
         verbose_name = "默写任务-教师端"
         verbose_name_plural = "默写任务-教师端"
@@ -54,30 +57,8 @@ class dictation_task_teacher(models.Model):
             self.status = '未开始'
         else:
             self.status = '已结束'
-        super().save(*args, **kwargs) #super()调用父类方法 .save()保存
-
+        super().save(*args, **kwargs)
         
-        task = dictation_task_student(task_name=self.task_name)
-        task.save()
-        task.contents.set(self.contents.all())
-        # 当ManytoManyField字段时，不可以直接赋值，要用set方法、
-            
-
-        # user_queryset1 = self.users.all()
-        # user_queryset2 = self.groups.user.all()
-        # combined_queryset = user_queryset1 | user_queryset2
-        # unique_users = combined_queryset.distinct()
-
-        # for content in self.contents.all():
-        #     for user in self.users.all():
-        #         print(f'Creating dictation_task_student {content} for user {user}')
-        #         dictation_task_student.objects.create(
-        #             task_name=self.task_name,
-        #             contents=content,
-        #             task_start_time=self.task_start_time,
-        #             task_end_time=self.task_end_time,
-        #             # user=user
-        #         )
 
     def remain_time(self):
         now = timezone.now()
@@ -90,18 +71,28 @@ class dictation_task_teacher(models.Model):
     remain_time.short_description = '剩余时间'
 
     def get_contents(self):
-        #将contents的内容以逗号分隔显示
         return ", ".join([content.content_name for content in self.contents.all()])
     get_contents.short_description = '默写内容'
 
-class dictation_task_student(models.Model):
+    def get_classes(self):
+        return ", ".join([Class.name for Class in self.classes.all()])
+    get_classes.short_description = '班级'
+
+    def get_students(self):
+        return ", ".join([student.name for student in self.students.all()])
+    get_students.short_description = '学生'
+
+class task_student(models.Model):
+
+    id              = models.AutoField(primary_key=True)
+    task_teacher    = models.ForeignKey(task_teacher,verbose_name='任务ID',on_delete=models.CASCADE,to_field='id',default=1)
     task_name       = models.CharField('默写任务名称',max_length=200,default='默写任务')
     contents        = models.ManyToManyField(content, verbose_name='古诗词库')
     task_start_time = models.DateTimeField('任务开始时间',default=get_default_end_time())
     task_end_time   = models.DateTimeField('任务结束时间',default=get_default_end_time(hours=1))
     status          = models.CharField('状态',max_length=200,default='未开始')
     score           = models.IntegerField('得分',default=0)
-    # user            = models.ForeignKey(User,verbose_name='学生',on_delete=models.CASCADE)
+    student         = models.ForeignKey('student',verbose_name='学生ID',on_delete=models.CASCADE,to_field='id',default=1)
     class Meta:
         verbose_name = "默写任务-学生端"
         verbose_name_plural = "默写任务-学生端"
@@ -133,4 +124,61 @@ class dictation_task_student(models.Model):
         else:
             return "已结束"
     remain_time.short_description = '剩余时间'
+
+class Class(models.Model):
+    id              = models.AutoField(primary_key=True)
+    name            = models.CharField('班级名称',max_length=200,default='未知')
+    class Meta:
+        verbose_name = "班级"
+        verbose_name_plural = "班级"
+
+    def __str__(self):
+        return self.name
+    
+class student(models.Model):
+    id              = models.AutoField(primary_key=True)
+    name            = models.CharField('学生姓名',max_length=200)
+    Class           = models.ForeignKey(Class,verbose_name='班级ID',on_delete=models.CASCADE,to_field='id',default=1)
+    class Meta:
+        verbose_name = "学生"
+        verbose_name_plural = "学生"
+
+    def __str__(self):
+        return self.name
+    
+
+@receiver(m2m_changed, sender=task_teacher.contents.through)
+@receiver(m2m_changed, sender=task_teacher.students.through)
+@receiver(m2m_changed, sender=task_teacher.classes.through)
+def add_task_student(sender, instance, **kwargs):
+    print("正在改变task_student")
+    print(instance.task_name)
+    print(instance.contents.all())
+    print(instance.students.all())
+    print(instance.classes.all()) 
+    #统计所给班级的所有学生
+    students = set()
+    if sender == task_teacher.classes.through:
+        for Class in instance.classes.all():
+            for student in Class.student_set.all():
+                students.add(student)
+    elif sender == task_teacher.students.through:
+        for student in instance.students.all():
+            students.add(student)
+
+    for student in students:
+        #如果task_teacher已经分发给该学生了，就不再分发
+        if task_student.objects.filter(task_teacher=instance,student=student).exists():
+            continue
+        task = task_student.objects.create(
+            task_teacher=instance,
+            task_name=instance.task_name,
+            task_start_time=instance.task_start_time,
+            task_end_time=instance.task_end_time,
+            student=student,
+        )
+        task.save()
+        task.contents.set(instance.contents.all())
+
+
 
